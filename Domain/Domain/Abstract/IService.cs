c3007133cd4fcaee99e97d5800ead6d058a1709d
AskerovAdil <Askerov121099@mail.ru>
2023-06-21 18:01:48 +0700

    feat: add category/product services

---
+++ Domain/Domain/Abstract/IService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Abstract
+{
+    public interface IService<T>
+    {
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+    }
+}
